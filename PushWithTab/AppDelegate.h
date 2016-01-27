3f412a42cf2e8da096fdd2bd548a9d4cedf046cb
soyoung <lanskyxti@163.com>
2016-01-27 14:26:59 +0800

    Initial Commit

---
+++ PushWithTab/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PushWithTab
+//
+//  Created by soyoung on 16/1/27.
+//  Copyright © 2016年 AlenW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PushWithTab/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PushWithTab
+//
+//  Created by soyoung on 16/1/27.
+//  Copyright © 2016年 AlenW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
